1654194d81997d29060779b9f06e1dad4906e0ba
Willie Richardson <willie.richardson72@gmail.com>
2015-10-30 15:34:31 -0700

    new save

--- game.js
+++ game.js
@@ -12,11 +12,13 @@
 function question1() {
   var question_one=prompt('Did Willie grow up in Seattle?').toUpperCase();
   if(question_one==="YES" || question_one==="Y") {
-    ans1.innerHTML = "Yes, Willie grew up in Seattle!" + '<br><img src="oct30images/Pearl-Jam-w20.jpg" width='200px'>';
+    ans1.innerHTML = "Yes, Willie grew up in Seattle!" + '<br><img src="oct30images/Pearl-Jam-w20.jpg" width=200px>';
+    ans1.className = "correct";
     console.log("You are correct to the question" + question_one);
     score+=1;
    } else {
       ans1.innerHTML = "No, you are not correct!";
+    ans1.className = "incorrect";
       console.log("You are incorrect to the question" + question_one);
    }
 }
@@ -24,11 +26,13 @@ function question1() {
 function question2() {
   var question_two=prompt("Does Willie enjoy the rain?").toUpperCase();
   if(question_two==="YES" || question_two==="Y") {
-    ans2.innerHTML = "Yes, Willie loves splashing in puddles" + '<br><img src="oct30images/tumblr_static_splashing_in_puddles_by_tigeress27-d3cd9rt.jpg" width='200px'>';
+    ans2.innerHTML = "Yes, Willie loves splashing in puddles" + '<br><img src="oct30images/tumblr_static_splashing_in_puddles_by_tigeress27-d3cd9rt.jpg" width=200px>';
+    ans2.className = "correct";
     console.log("You are correct to the question" + question_two);
     score+=1;
    } else {
     ans2.innerHTML = "No, you are not correct!";
+    ans2.className = "incorrect";
     console.log("You are incorrect to the question" + question_two);
    }
 }
@@ -36,11 +40,13 @@ function question2() {
 function question3() {
   var question_three=prompt("Does Willie believe in the Sasquatch?").toUpperCase();
   if (question_three==="YES" || question_three==="Y") {
-    ans3.innerHTML = "Yes, he has seen the Northwest Legend!" + '<br><img id="answer3yes" src="sask.jpg" width='200px'>;
+    ans3.innerHTML = "Yes, he has seen the Northwest Legend!" + '<br><img src="oct30images/sask.jpg" width=200px>';
+    ans3.className = "correct";
     console.log("You are correct to the question" + question_three);
     score+=1;
    } else {
     ans3.innerHTML = "No, you are not correct!";
+    ans3.className = "incorrect";
     console.log("You are incorrect to the question" + question_three);
    }
 }
@@ -48,11 +54,13 @@ function question3() {
 function question4() {
   var question_four=prompt("Has Willie been to a chicken sacrifice as an offering to Mayan Gods?").toUpperCase();
   if (question_four==="YES" || question_four==="Y") {
-    ans4.innerHTML = "Yes, he attended!";
+    ans4.innerHTML = "Yes, he attended!" + '<br><img src="oct30images/chicken.jpg" width=200px>';
+    ans4.className = "correct";
     console.log("You are correct to the question" + question_four);
     score+=1;
    } else {
     ans4.innerHTML = "No, you are not correct!";
+    ans4.className = "incorrect";
     console.log("You are incorrect to the question" + question_four);
    }
 }
@@ -60,11 +68,13 @@ function question4() {
 function question5() {
   var question_five=prompt("Does Willie only have one kidney?").toUpperCase();
   if (question_five==="YES" || question_five==="Y") {
-    ans5.innerHTML = "Yes, he has a larger right kidney to compensate!";
+    ans5.innerHTML = "Yes, he has a larger right kidney to compensate!" + '<br><img src="oct30images/kidney.jpg" width=200px>';
+    ans5.className = 'correct';
     console.log("You are correct to the question" + question_five);
     score+=1;
    } else {
     ans5.innerHTML = "No, you are not correct!";
+    ans5.className = 'incorrect';
     console.log("You are incorrect to the question" + question_five);
    }
  }
@@ -72,18 +82,20 @@ function question5() {
 function question6 () {
   var question_six=prompt("Was Willie a member of Boy Scout Troop 666?").toUpperCase();
   if (question_six==="YES" || question_six==="Y") {
-    ans6.innerHTML = "Yes, he was a member!";
+    ans6.innerHTML = "Yes, he was a member!" + '<br><img src="oct30images/acdc.jpg" width=200px>';
+    ans6.className = "correct";
     console.log("You are correct to the question" + question_six);
     score+=1;
    } else {
-    ans6.innerHTML ="No, you are not correct!";
+    ans6.innerHTML = "No, you are not correct!";
+    ans6.className = "incorect";
     console.log("You are incorrect to the question" + question_six);
    }
-}
+};
 
 question1();
-window.setTimeout(question2, 500);
-window.setTimeout(question3, 500);
+window.setTimeout(question2, 100);
+window.setTimeout(question3, 100);
 window.setTimeout(question4, 500);
 window.setTimeout(question5, 500);
 window.setTimeout(question6, 500);
